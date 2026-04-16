7a9d7f098370a472fc97a5838ff6c4e1ba7aad42
Your Name <you@example.com>
2026-04-16 16:59:52 -0300

    Iniciando meu Projeto

---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,25 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			//Variaveis
+		int idade = 24;
+		String nome = "Dayana";
+		float graus = 20.0f;
+		boolean resposta;
+		
+		
+			//Constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double ÁREA = 3.1415;
+		
+		
+		
+		
+	}
+
+}
