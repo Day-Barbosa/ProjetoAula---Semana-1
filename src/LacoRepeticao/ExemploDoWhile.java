a8f9fee2e27e3b0cd71cec2ebe5cba567e376dc5
Your Name <you@example.com>
2026-04-24 22:32:38 -0300

    Conteudo das aulas - Introducao, EntradaSaida, LacoRepeticao, Vetor, Collections

---
+++ src/LacoRepeticao/ExemploDoWhile.java
@@ -0,0 +1,23 @@
+package LacoRepeticao;
+
+import java.util.Scanner;
+
+public class ExemploDoWhile {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+        int numero, resultado, contador = 3;
+        
+        do {
+		
+		System.out.println("\nDigite um número inteiro: ");
+        numero = leia.nextInt();
+        resultado = numero * 5;
+        System.out.println("\nO resultado da multiplicação é: " + resultado);
+
+        } while (contador <= 2);
+
+	}
+
+}
